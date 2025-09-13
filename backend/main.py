b75e478822b13e1f4198520cd0e2a62f2910c166
Pratyush Kumar <prtsh2005@gmail.com>
2025-09-14 04:58:49 +0530

    abcd

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from sqlmodel import SQLModel
+from database import engine
+from src.models.models import Herb
+
+
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
-
-# Define input data structure
-class InputData(BaseModel):
-    feature1: float
-    feature2: float
-
-# A new endpoint for prediction
-@app.post("/predict")
-def predict(data: InputData):
-    # Temporary fake prediction
-    result = data.feature1 + data.feature2
-    return {"prediction": result}
+    return {"message": "Hello from FastAPI with DB!"}
